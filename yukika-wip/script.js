b0c5f5572ad88732f6bd3383e6341e68082e0b51
Desteny <desteny282@gmail.com>
2024-07-17 23:53:42 -0400

    yukika WIP

--- yukika-wip/script.js
+++ yukika-wip/script.js
@@ -1,1 +1,8 @@
+	let passport = document.getElementById("cover");
+	let pageOne = document.getElementById("page-1");
 
+	function passportOpen() {
+		passport.className = "passportANI";
+		pageOne.className = "pageANI";
+		pageOne.style.display = "block";
+	}
