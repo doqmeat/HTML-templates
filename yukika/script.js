dc987d552b5c0f3d66410cb1df3a0c506339b3cd
Desteny <desteny282@gmail.com>
2024-08-16 21:24:54 -0400

    yukika template

--- yukika/script.js
+++ yukika/script.js
@@ -1,16 +1,24 @@
 	let passport = document.getElementById("cover");
+	let passportOpened = document.getElementById("passport-opened");
 	let page1 = document.getElementById("page-1");
 	let page2 = document.getElementById("page-2");
 	let btn1 = document.getElementById("beforeBtn");
 	let btn2 = document.getElementById("afterBtn");
+	let otherPages = document.getElementsByClassName("page");
+	let i = 0;
 
 
 	function passportOpen() {
-		btn1.style.display = "block";
-		btn2.style.display = "block";
-		passport.className = "passportANI";
-		page1.className = "pageANI";
+		btn1.style.display = "inline-block";
+		btn2.style.display = "inline-block";
+		//		pages
+		passportOpened.style.display = "block";
 		page1.style.display = "block";
+		page2.style.display = "none";
+		//		animations
+		passport.className = "passportANI";
+		passportOpened.className = "pageANI";
+		//		buttons
 		btn1.onclick = passportClose;
 		btn1.innerText = "close passport";
 		btn2.onclick = passportPage2;
@@ -20,8 +28,8 @@
 	function passportPage1() {
 		page1.style.display = "block";
 		page2.style.display = "none";
-		page1.style.opacity = "1";
-		page1.classList.remove("pageANI");
+		passportOpened.style.opacity = "1";
+		passportOpened.classList.remove("pageANI");
 		btn1.onclick = passportClose;
 		btn1.innerText = "close passport";
 		btn2.onclick = passportPage2;
@@ -32,7 +40,6 @@
 	function passportPage2() {
 		page1.style.display = "none";
 		page2.style.display = "block";
-		page2.style.opacity = "1";
 		btn1.onclick = passportPage1;
 		btn1.innerText = "previous page";
 		btn2.onclick = passportClose;
@@ -41,10 +48,9 @@
 	}
 
 	function passportClose() {
-		passport.style.display = "block";
+	passport.style.display = "block";
 		passport.className = "passportCloseANI";
-		page2.style.display = "none";
-		page1.style.display = "none";
+		passportOpened.style.display = "none";
 		btn1.style.display = "none";
 		btn2.style.display = "none";
 	}
